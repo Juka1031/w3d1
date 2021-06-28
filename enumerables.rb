9f01a7b05dae6e79df4490f6ad962b8038e9b822
alexanderlam <skalexlam@gmail.com>
2021-06-28 16:43:59 -0400

    up to my_select

--- enumerables.rb
+++ enumerables.rb
@@ -1,2 +1,24 @@
-#alex test
-#julian test
+class Array
+    def my_each(&prc)
+        i = 0
+        while i < self.length
+            prc.call(self[i])
+            i += 1
+        end
+
+        self
+    end
+
+    def my_select(&prc) #  if num > 1
+        newArr = [] 
+        self.my_each do |ele|   # 1, 2, 3
+            newArr << ele if prc.call(ele) # 
+        end
+
+        newArr
+    end
+end
+
+a = [1, 2, 3]
+p a.my_select { |num| num > 1 } # => [2, 3]
+p a.my_select { |num| num == 4 } # => []
