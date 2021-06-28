220274dc0732ae2dc582a19627595dbae98d79a6
julian <juka1031@gmail.com>
2021-06-28 16:51:34 -0400

    array part

--- enumerables.rb
+++ enumerables.rb
@@ -17,8 +17,30 @@ class Array
 
         newArr
     end
+
+    def my_reject(&block)
+        new_arr = []
+        self.my_each do |ele|
+            new_arr << ele if !block.call(ele)
+        end
+        new_arr
+    end
+
+    def my_any?(&block) #return boolean
+        self.each do |ele|
+            return true if block.call(ele)
+        end
+        false
+    end
+
+    def my_all?(&block)
+        self.each do |ele|
+            return false if !block.call(ele)
+        end
+        true
+    end
 end
 
 a = [1, 2, 3]
-p a.my_select { |num| num > 1 } # => [2, 3]
-p a.my_select { |num| num == 4 } # => []
+p a.my_all? { |num| num > 1 } # => false
+p a.my_all? { |num| num < 4 } # => true
